b8db6514aef036a5e9282422f0370e076dc078e0
Freddy Drennan <drennanfreddy@gmail.com>
2021-02-08 16:38:30 -0700

    update

--- make.sh
+++ make.sh
@@ -6,25 +6,25 @@ cp main.R ./r-base/
 cp -r sql ./r-base/
 cp plumber.R ./r-base/
 
-echo "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > airflow/.env
+echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > airflow/.env
 
 docker volume prune
 
-mkdir -p airflow/plugins
-mkdir -p airflow/logs
-mkdir -p airflow/dags
+# mkdir -p airflow/plugins
+# mkdir -p airflow/logs
+# mkdir -p airflow/dags
 
-sudo chmod 777 airflow/plugins
-sudo chmod 777 airflow/logs
-sudo chmod 777 airflow/dags
+# sudo chmod 777 airflow/plugins
+# sudo chmod 777 airflow/logs
+# sudo chmod 777 airflow/dags
 
 docker-compose -f postgres/docker-compose.yaml down
-docker-compose -f airflow/docker-compose.yaml down
+# docker-compose -f airflow/docker-compose.yaml down
 docker-compose -f r-base/docker-compose.yaml down
 
-docker-compose -f airflow/docker-compose.yaml up airflow-init
+# docker-compose -f airflow/docker-compose.yaml up airflow-init
 docker-compose -f r-base/docker-compose.yaml build
 
 docker-compose -f postgres/docker-compose.yaml up -d
-docker-compose -f airflow/docker-compose.yaml up -d
+# docker-compose -f airflow/docker-compose.yaml up -d
 docker-compose -f r-base/docker-compose.yaml up -d
