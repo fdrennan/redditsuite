c70a38746daf61dbf133ee5e0e6ad5a0aa7c1b7a
Freddy Drennan <drennanfreddy@gmail.com>
2021-02-08 15:25:21 -0700

    update

--- make.sh
+++ make.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-cp .Renviron ./r-base/.Renviron
-cp renv.lock ./r-base/renv.lock
-cp main.R ./r-base/main.R
-rm -r ./r-base/sql
+cp .Renviron ./r-base/
+cp renv.lock ./r-base/
+cp main.R ./r-base/
 cp -r sql ./r-base/
+cp plumber.R ./r-base/
+
+echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > airflow/.env
+
+docker-compose -f airflow/docker-compose.yaml down
+docker-compose -f r-base/docker-compose.yaml down
+
+docker-compose -f airflow/docker-compose.yaml up airflow-init
 docker-compose -f r-base/docker-compose.yaml build
+
+docker-compose -f airflow/docker-compose.yaml up -d
+docker-compose -f r-base/docker-compose.yaml up -d
