d055bba55fd4a06976d51892036157fbca359883
Freddy Drennan <drennanfreddy@gmail.com>
2021-02-18 14:44:43 -0700

    updated postgres function

--- R/connection_postgres.R
+++ R/connection_postgres.R
@@ -1,11 +1,20 @@
 #' connection_postgres
-#' @zydevparam POSTGRES_HOST host.docker.internal
+#' @description Adds the ability to re-try connections on failure and reads from
+#' pre-defined environment variables names.
+#' @param POSTGRES_HOST for the host name
+#' @param POSTGRES_PORT for the port number
+#' @param POSTGRES_DB for the name of the database on the host, or the database file name
+#' @param POSTGRES_USER for the user name
+#' @param POSTGRES_PASSWORD for the password
+#' @param n_connection_tries Number of times to try connecting to PostgreSQL
 #' @export connection_postgres
 connection_postgres <- function(POSTGRES_HOST = NULL,
                                 POSTGRES_PORT = NULL,
                                 POSTGRES_USER = NULL,
                                 POSTGRES_PASSWORD = NULL,
-                                POSTGRES_DB = NULL) {
+                                POSTGRES_DB = NULL,
+                                n_connection_tries = 5) {
+
   if (is.null(POSTGRES_HOST)) {
     POSTGRES_HOST <- Sys.getenv("POSTGRES_HOST")
   }
@@ -29,6 +38,7 @@ connection_postgres <- function(POSTGRES_HOST = NULL,
   n <- 1
   message("First attempt at connection")
   repeat {
+
     connection <- try({
       dbConnect(RPostgres::Postgres(),
         host = POSTGRES_HOST,
@@ -39,10 +49,10 @@ connection_postgres <- function(POSTGRES_HOST = NULL,
       )
     })
 
-    if (!inherits(connection, "try-error")) {
+    if (inherits(connection, "PqConnection")) {
       break
     } else {
-      if (n > 5) {
+      if (n > n_connection_tries) {
         stop("Database connection failed")
       }
       n <- n + 1
